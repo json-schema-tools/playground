5d4b87f36e4bcdc510d55e98d2a6c3cd49fa02e7
shanejonas <jonas.shane@gmail.com>
2020-02-24 17:29:20 -0800

    feat: initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,50 @@
+## [1.0.1](https://github.com/etclabscore/pristine-typescript-react-material-ui-monaco-playground/compare/1.0.0...1.0.1) (2019-11-27)
+
+
+### Bug Fixes
+
+* **package.json:** homepage link ([e854587](https://github.com/etclabscore/pristine-typescript-react-material-ui-monaco-playground/commit/e8545870e57ac00078e0ae9cf7c0e9ca777d7a45))
+
+# 1.0.0 (2019-11-27)
+
+
+### Bug Fixes
+
+* linting ([c651584](https://github.com/etclabscore/pristine-typescript-react-material-ui-monaco-playground/commit/c651584e4cea12bddcc8bc1d4e29a8d60d9fd28f))
+* **README:** update content to monaco playground ([f6b83bb](https://github.com/etclabscore/pristine-typescript-react-material-ui-monaco-playground/commit/f6b83bb24da1be50d1d05a214098b838820771ee))
+* remove unused variables ([946ff9d](https://github.com/etclabscore/pristine-typescript-react-material-ui-monaco-playground/commit/946ff9d04b4cfeb3fd1763b272d7db0fca5b1675))
+
+# 1.0.0 (2019-10-24)
+
+
+### Bug Fixes
+
+* add demo gif to README ([a1e0ce6](https://github.com/etclabscore/pristine-typescript-react-material-ui/commit/a1e0ce6dd7c0d44e46e41faaf52b7e45b8623ce9))
+* add viewport meta ([6843410](https://github.com/etclabscore/pristine-typescript-react-material-ui/commit/68434105895ea915c3aa4204c8827801d3a5d7bc))
+* linting ([27c26d6](https://github.com/etclabscore/pristine-typescript-react-material-ui/commit/27c26d6fa744910a0c53789b0f020a7870053925))
+
+## [1.0.2](https://github.com/etclabscore/pristine-typescript-react/compare/1.0.1...1.0.2) (2019-07-02)
+
+
+### Bug Fixes
+
+* add homepage to fix gh-pages ([dcb2099](https://github.com/etclabscore/pristine-typescript-react/commit/dcb2099))
+
+## [1.0.1](https://github.com/etclabscore/pristine-typescript-react/compare/1.0.0...1.0.1) (2019-07-02)
+
+
+### Bug Fixes
+
+* **.releaserc:** remove npm publish ([2ee1fce](https://github.com/etclabscore/pristine-typescript-react/commit/2ee1fce))
+
+# 1.0.0 (2019-07-02)
+
+
+### Bug Fixes
+
+* **jest:** use passWithNoTests ([f7fdd6c](https://github.com/etclabscore/pristine-typescript-react/commit/f7fdd6c))
+
+
+### Features
+
+* initial commit ([2cf2f38](https://github.com/etclabscore/pristine-typescript-react/commit/2cf2f38))
---
+++ src/containers/MyApp.tsx
@@ -0,0 +1,134 @@
+import React, { useState, useRef, useEffect } from "react";
+import { MuiThemeProvider, AppBar, Toolbar, Typography, IconButton, Tooltip, CssBaseline, Grid, Menu, Button, MenuItem } from "@material-ui/core"; //tslint:disable-line
+import useDarkMode from "use-dark-mode";
+import Brightness3Icon from "@material-ui/icons/Brightness3";
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import WbSunnyIcon from "@material-ui/icons/WbSunny";
+import { lightTheme, darkTheme } from "../themes/theme";
+import { useTranslation } from "react-i18next";
+import LanguageMenu from "./LanguageMenu";
+import SplitPane from "react-split-pane";
+import JsonSchemaToTypes, { SupportedLanguages } from "@etclabscore/json-schema-to-types";
+import "./MyApp.css";
+import PlayCircle from "@material-ui/icons/PlayCircleFilled";
+import Editor from "@etclabscore/react-monaco-editor";
+
+const languages: SupportedLanguages[] = ["typescript", "golang", "python", "rust"];
+
+const MyApp: React.FC = () => {
+  const darkMode = useDarkMode();
+  const [isEditorReady, setIsEditorReady] = useState(false);
+  const [defaultValue] = useState(`{\n  "title": "foo",\n  "type": "string" \n}`)
+  const [value, setValue] = useState(defaultValue);
+  const [results, setResults] = useState("");
+  const [languageAnchorEl, setLanguageAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [selectedLanguage, setSelectedLanguage]: [SupportedLanguages, any] = useState("typescript");
+  const [transpiler, setTranspiler]: [JsonSchemaToTypes | undefined, any] = useState();
+
+  function handleTranspile() {
+    try {
+      const result = JSON.parse(value);
+      const tr = new JsonSchemaToTypes(result);
+      setTranspiler(tr);
+      setResults(tr.to(selectedLanguage));
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  const { t } = useTranslation();
+  const theme = darkMode.value ? darkTheme : lightTheme;
+
+  const handleLanguageClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setLanguageAnchorEl(event.currentTarget);
+  };
+  const handleLanguageClose = () => {
+    setLanguageAnchorEl(null);
+  };
+  const handleLanguageChange = (lang: string) => {
+    setSelectedLanguage(lang);
+    setLanguageAnchorEl(null);
+  };
+
+  const handleEditorDidMount = (editor: any) => {
+    setIsEditorReady(true);
+  };
+
+  useEffect(() => {
+    handleTranspile();
+  }, [value, selectedLanguage]);
+
+  return (
+    <MuiThemeProvider theme={theme}>
+      <AppBar position="static" color="default" elevation={0}>
+        <Toolbar>
+          <Grid container alignContent="center" alignItems="center" justify="flex-start">
+            <Typography variant="h6" style={{ paddingRight: "20px" }}>{t("json-schema.tools")}</Typography>
+            <Typography variant="caption" style={{ paddingRight: "5px" }}>
+              {t("playground")}
+            </Typography>
+          </Grid>
+          <Grid container alignContent="center" alignItems="center" justify="flex-end">
+            {<>
+              <Tooltip title={"Language"}>
+                <>
+                  <Typography variant="body1" style={{ paddingRight: "10px" }}>Language:</Typography>
+                  <Button onClick={handleLanguageClick} variant="outlined" endIcon={<ArrowDropDownIcon />}>{selectedLanguage}</Button>
+                </>
+              </Tooltip>
+              <Menu
+                id="input-menu"
+                anchorEl={languageAnchorEl}
+                keepMounted
+                open={Boolean(languageAnchorEl)}
+                onClose={handleLanguageClose}
+              >
+                {Object.values(languages).map((language, i) => (
+                  <MenuItem onClick={(event) => handleLanguageChange(language)}>{language}</MenuItem>
+                ))}
+              </Menu>
+            </>
+            }
+            <Tooltip title={t("Toggle Dark Mode")}>
+              <IconButton onClick={darkMode.toggle}>
+                {darkMode.value ? <Brightness3Icon /> : <WbSunnyIcon />}
+              </IconButton>
+            </Tooltip>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <CssBaseline />
+      <SplitPane split="vertical" minSize={100} maxSize={-100} defaultSize={"35%"} style={{ flexGrow: 1 }}>
+        <Editor
+          height="90vh"
+          value={defaultValue}
+          onChange={(ev: any, v: string) => {
+            setValue(v);
+          }}
+          editorDidMount={handleEditorDidMount}
+          language="json"
+        />
+        <Editor
+          height="90vh"
+          editorDidMount={handleEditorDidMount}
+          options={{
+            minimap: {
+              enabled: false,
+            },
+            wordWrap: "on",
+            lineNumbers: "off",
+            wrappingIndent: "deepIndent",
+            readOnly: true,
+            showFoldingControls: "always",
+          }}
+          value={results}
+          language={selectedLanguage}
+        />
+        <div>
+        </div>
+      </SplitPane>
+    </MuiThemeProvider>
+  );
+};
+
+export default MyApp;
